187a1f7857a260827bdfe153812d3d237ea45ced
NSDongHyeon <37198090+NSDongHyeon@users.noreply.github.com>
2018-03-09 11:28:14 +0900

    Update NsCommUtil.java

--- NsCommUtil.java
+++ NsCommUtil.java
@@ -12,10 +12,10 @@ import java.util.Map;
 /**
  * @Class Name :	NsCommUtil.java
  * @Description :	사용 빈도가 높은 함수들을 이 유틸리티 클래스안에 메소드로 구현하고 서로 공유하기 위함이며, 
- * 						Contents에 적힌 번호로 관련 함수를 검색하여 사용.
+ * 			Contents에 적힌 번호로 관련 함수를 검색하여 사용.
  * @Contents
- * 	- NS001	:	Null, 공백처리
- *	- NS002	:	데이터 파싱
+ * 	- NS001	: Null, 공백처리
+ *	- NS002	: 데이터 파싱
  * 
  * @see Copyright ⓒ 2018. NsHomeShopping Corp.
  */
